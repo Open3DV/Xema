46f89a8b302efe77595f793e6f323d409dd77250
zhangguanjin <542493073@qq.com>
2024-04-18 12:03:30 +0800

    git version版本信息格式保持跟旧版本一致。

--- version/build.sh
+++ version/build.sh
@@ -5,4 +5,4 @@ current_time=`date +"%Y-%m-%d %H:%M:%S"`
 git_version=`git log -1 --format="%h"`
 camera_model=''
 git_tag=`git describe --tags --abbrev=0` 
-sed s/MYVERSION/"version: $git_version\ commit: $commit_time\ build: $current_time"/g version.h.tmp > ../firmware/version.h
+sed s/MYVERSION/"XEMA version: $git_version\ commit: $commit_time\ build: $current_time"/g version.h.tmp > ../firmware/version.h
